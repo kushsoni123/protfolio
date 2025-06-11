5c4f396928afa7d076c51f1ed2d61addb3e738b0
kushsoni123 <kushsoni6262@gmail.com>
2025-06-11 22:19:44 +0530

    kush

--- script.js
+++ script.js
@@ -2,10 +2,3 @@ var typed = new Typed('#element', {
     strings: ['web developer', 'web designer', 'Frontend developer'],
     typeSpeed: 50,
 });
-// const colors = ['#FF5733', '#33FF57', '#3357FF', '#FF33A6', '#FFFF33']; // Example colors
-// let index = 0;
-
-// setInterval(() => {
-//     document.body.style.backgroundColor = colors[index];
-//     index = (index + 1) % colors.length;
-// }, 2000); // Changes every 2 seconds
